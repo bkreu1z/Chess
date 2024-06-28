bacd9262a008ea20e25a66119a981fa913940c88
bkreu1z <bkreu1z@byu.edu>
2024-06-28 15:17:52 -0600

    Added override functions to position and passed the tests

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -12,8 +12,8 @@ public class ChessPosition {
     public ChessPiece piece;
 
     public ChessPosition(int row, int col) {
-        this.y = row;
-        this.x = col;
+        y = row;
+        x = col;
     }
 
     /**
@@ -31,4 +31,27 @@ public class ChessPosition {
     public int getColumn() {
         return this.x;
     }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);//may want to add functionality to check if the piece is the same
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ChessPosition other = (ChessPosition) obj;
+        return this.x == other.x && this.y == other.y && this.piece == other.piece;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.x * 31 + this.y;
+    }
+
 }
