17a653db6c416d7510c045a670993cae278111fe
bkreu1z <bkreu1z@byu.edu>
2024-07-01 21:58:47 -0600

    Change to location of checking if a pawn can move two

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -11,11 +11,11 @@ public class PawnMoveCalculator implements MoveCalculator {
         int y = myPosition.getRow();
         try {
             if (myPosition.piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                if (myPosition.getRow() == 7 && board.spaces[x - 1][y - 3].isEmpty()) {//if it hasn't moved yet
-                    moves.add(new ChessPosition(x, y - 2));
-                }
                 if (board.spaces[x - 1][y - 2].isEmpty()) {
                     moves.add(new ChessPosition(x, y - 1));
+                    if (myPosition.getRow() == 7 && board.spaces[x - 1][y - 3].isEmpty()) {//if it hasn't moved yet
+                        moves.add(new ChessPosition(x, y - 2));
+                    };
                 }
                 if (!board.spaces[x - 2][y - 2].isEmpty() && board.spaces[x - 2][y - 2].piece.getTeamColor() == ChessGame.TeamColor.WHITE) {//check left diagonal
                     moves.add(new ChessPosition(x - 1, y - 1));
@@ -24,11 +24,11 @@ public class PawnMoveCalculator implements MoveCalculator {
                     moves.add(new ChessPosition(x + 1, y - 1));
                 }
             } else {//if the pawn is white
-                if (myPosition.getRow() == 2 && board.spaces[x - 1][y + 1].isEmpty()) {//if it hasn't moved yet
-                    moves.add(new ChessPosition(x, y + 2));//check indexes on this
-                }
                 if (board.spaces[x - 1][y].isEmpty()) {
                     moves.add(new ChessPosition(x, y + 1));
+                    if (myPosition.getRow() == 2 && board.spaces[x - 1][y + 1].isEmpty()) {//if it hasn't moved yet
+                        moves.add(new ChessPosition(x, y + 2));
+                    }
                 }
                 if (!board.spaces[x - 2][y].isEmpty() && board.spaces[x - 2][y].piece.getTeamColor() == ChessGame.TeamColor.WHITE) {//check left diagonal
                     moves.add(new ChessPosition(x - 1, y + 1));
