bc6d19d6f3eab47f093f3a281b8a502870f7e422
bkreu1z <bkreu1z@byu.edu>
2024-07-01 19:55:24 -0600

    Implemented QueenMoveCalculator

--- shared/src/main/java/chess/QueenMoveCalculator.java
+++ shared/src/main/java/chess/QueenMoveCalculator.java
@@ -1,11 +1,15 @@
 package chess;
 
+import java.util.HashSet;
 import java.util.Set;
 
-public class QueenMoveCalculator implements MoveCalculator {
+public class QueenMoveCalculator extends StraightLinesMoveCalculator implements MoveCalculator {
     @Override
     public Set<ChessPosition> findMoves(ChessBoard board, ChessPosition myPosition) {
-        return Set.of();
+        Set<ChessPosition> moves = new HashSet<>();
+        moves.addAll(super.findDiagonalMoves(board, myPosition));
+        moves.addAll(super.findOctMoves(board, myPosition));
+        return moves;
     }
 }
 
