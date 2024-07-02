6714dbf17014c1984d5ad463f6b8c881ae484b7b
bkreu1z <bkreu1z@byu.edu>
2024-07-01 19:41:42 -0600

    Implemented RookMoveCalculator

--- shared/src/main/java/chess/RookMoveCalculator.java
+++ shared/src/main/java/chess/RookMoveCalculator.java
@@ -2,9 +2,9 @@ package chess;
 
 import java.util.Set;
 
-public class RookMoveCalculator implements MoveCalculator {
+public class RookMoveCalculator extends StraightLinesMoveCalculator implements MoveCalculator {
     @Override
     public Set<ChessPosition> findMoves(ChessBoard board, ChessPosition myPosition) {
-        return Set.of();
+        return super.findOctMoves(board, myPosition);
     }
 }
--- shared/src/main/java/chess/StraightLinesMoveCalculator.java
+++ shared/src/main/java/chess/StraightLinesMoveCalculator.java
@@ -1,6 +1,55 @@
 package chess;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public abstract class StraightLinesMoveCalculator {
+    public Set<ChessPosition> findOctMoves(ChessBoard board, ChessPosition myPosition) {
+        Set<ChessPosition> octMoves = new HashSet<>();
+        int currentx = myPosition.getColumn();
+        int currenty = myPosition.getRow();
+        int left = myPosition.getColumn() - 1;
+        int right = myPosition.getColumn() + 1;
+        int up = myPosition.getRow() + 1;
+        int down = myPosition.getRow() - 1;
+        while (up <= 8) {
+            if (board.spaces[currentx - 1][up - 1].isEmpty()){
+                octMoves.add(new ChessPosition(currentx, up));
+            } else if (myPosition.piece.getTeamColor() != board.spaces[currentx - 1][up - 1].piece.getTeamColor()) {
+                octMoves.add(new ChessPosition(currentx, up));
+                break;
+            }
+            up++;
+        }
+        while (down > 0) {
+            if (board.spaces[currentx - 1][down - 1].isEmpty()){
+                octMoves.add(new ChessPosition(currentx, down));
+            } else if (myPosition.piece.getTeamColor() != board.spaces[currentx - 1][down - 1].piece.getTeamColor()) {
+                octMoves.add(new ChessPosition(currentx, down));
+                break;
+            }
+            down--;
+        }
+        while (left > 0) {
+            if (board.spaces[left - 1][currenty - 1].isEmpty()){
+                octMoves.add(new ChessPosition(left, currenty));
+            } else if (myPosition.piece.getTeamColor() != board.spaces[left - 1][currenty - 1].piece.getTeamColor()) {
+                octMoves.add(new ChessPosition(left, currenty));
+                break;
+            }
+            left--;
+        }
+        while (right <= 8) {
+            if (board.spaces[right - 1][currenty - 1].isEmpty()){
+                octMoves.add(new ChessPosition(right, currenty));
+            } else if (myPosition.piece.getTeamColor() != board.spaces[right - 1][currenty - 1].piece.getTeamColor()) {
+                octMoves.add(new ChessPosition(right, currenty));
+                break;
+            }
+            right++;
+        }
+        return octMoves;
+    }
+
+    public Set<ChessPosition> findDiagonalMoves(ChessBoard board, ChessPosition myPosition) {}
 }
