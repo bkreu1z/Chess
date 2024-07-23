67413c82eea4e3095dd5b820f9adf4e561a2bc19
bkreu1z <bkreu1z@byu.edu>
2024-07-23 14:44:14 -0600

    Created more handler classes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,16 +10,16 @@ public class Server {
 
         Spark.staticFiles.location("web");
         ClearHandler clearHandler = new ClearHandler();
+        RegisterHandler
 
         // Register your endpoints and handle exceptions here.
-        //Spark.post("/user", new RegisterHandler());//classes need to implement Route interface
+        //Spark.post("/user", new RegisterHandler());
         //Spark.post("/session", new LoginHandler());
         //Spark.post("/game", new CreateGameHandler());
         //Spark.delete("/session", new DeleteGameHandler());
         Spark.delete("/db", clearHandler::handle);
         //Spark.get("/game", new ListGamesHandler());
         //Spark.put("/game", new JoinGameHandler());
-        //Spark.delete
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
