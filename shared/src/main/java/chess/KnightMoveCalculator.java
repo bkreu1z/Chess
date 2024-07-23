f41081e8e673e5c85981c507ef38dc1d847c2bef
bkreu1z <bkreu1z@byu.edu>
2024-07-23 08:10:10 -0600

    Cleaned up KnightMoveCalculator so the code doesn't repeat

--- shared/src/main/java/chess/KnightMoveCalculator.java
+++ shared/src/main/java/chess/KnightMoveCalculator.java
@@ -6,49 +6,27 @@ import java.util.Set;
 public class KnightMoveCalculator implements MoveCalculator {
     @Override
     public Set<ChessPosition> findMoves(ChessBoard board, ChessPosition myPosition) {
-        Set<ChessPosition> moves = new HashSet<ChessPosition>();
+        Set<ChessPosition> moves = new HashSet<>();
         int y = myPosition.getRow();
         int x = myPosition.getColumn();
-        if (x + 1 <= 8 && y + 2 <= 8) {
-            if (board.getPiece(new ChessPosition(y + 2, x + 1)) == null || board.getPiece(new ChessPosition(y + 2, x + 1)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y + 2, x + 1));
-            }
-        }
-        if (x + 2 <= 8 && y + 1 <= 8) {
-            if (board.getPiece(new ChessPosition(y + 1, x + 2)) == null || board.getPiece(new ChessPosition(y + 1, x + 2)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y + 1, x + 2));
-            }
-        }
-        if (x + 2 <= 8 && y - 1 >= 1) {
-            if (board.getPiece(new ChessPosition(y - 1, x + 2)) == null || board.getPiece(new ChessPosition(y - 1, x + 2)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y - 1, x + 2));
-            }
-        }
-        if (x + 1 <= 8 && y - 2 >= 1) {
-            if (board.getPiece(new ChessPosition(y - 2, x + 1)) == null || board.getPiece(new ChessPosition(y - 2, x + 1)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y - 2, x + 1));
-            }
-        }
-        if (x - 1 >= 1 && y - 2 >= 1) {
-            if (board.getPiece(new ChessPosition(y - 2, x - 1)) == null || board.getPiece(new ChessPosition(y - 2, x - 1)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y - 2, x - 1));
-            }
-        }
-        if (x - 2 >= 1 && y - 1 >= 1) {
-            if (board.getPiece(new ChessPosition(y - 1, x - 2)) == null || board.getPiece(new ChessPosition(y - 1, x - 2)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y - 1, x - 2));
-            }
-        }
-        if (x - 2 >= 1 && y + 1 <= 8) {
-            if (board.getPiece(new ChessPosition(y + 1, x - 2)) == null || board.getPiece(new ChessPosition(y + 1, x - 2)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y + 1, x - 2));
-            }
-        }
-        if (x - 1 >= 1 && y + 2 <= 8) {
-            if (board.getPiece(new ChessPosition(y + 2, x - 1)) == null || board.getPiece(new ChessPosition(y + 2, x - 1)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                moves.add(new ChessPosition(y + 2, x - 1));
-            }
-        }
+        ChessGame.TeamColor teamcolor = board.getPiece(myPosition).getTeamColor();
+        addIfValid(x + 1, y + 2, board, moves, teamcolor);
+        addIfValid(x + 2, y + 1, board, moves, teamcolor);
+        addIfValid(x + 2, y - 1, board, moves, teamcolor);
+        addIfValid(x + 1, y - 2, board, moves, teamcolor);
+        addIfValid(x - 1, y - 2, board, moves, teamcolor);
+        addIfValid(x - 2, y - 1, board, moves, teamcolor);
+        addIfValid(x - 2, y + 1, board, moves, teamcolor);
+        addIfValid(x - 1, y + 2, board, moves, teamcolor);
         return moves;
+    }
+
+    public void addIfValid(int newX, int newY, ChessBoard board, Set<ChessPosition> moves, ChessGame.TeamColor teamColor) {
+        if (1 <= newX && newX <= 8 && 1 <= newY && newY <= 8) {
+            ChessPosition position = new ChessPosition(newY, newX);
+            if (board.getPiece(position) == null || board.getPiece(position).getTeamColor() != teamColor) {
+                moves.add(position);
+            }
+        }
     }
 }
