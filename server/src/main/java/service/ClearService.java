5b899c55eaf228bc0f815a8aeb21932ddda7c949
bkreu1z <bkreu1z@byu.edu>
2024-07-23 11:00:20 -0600

    Wrote basic implementation for clear service

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,16 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+
 public class ClearService {
+    GameDAO gameDAO = new GameDAO();
+    UserDAO userDAO = new UserDAO();
+    AuthDAO authDAO = new AuthDAO();
+
+    public void clear() {
+        gameDAO.clear();
+        userDAO.clear();
+        authDAO.clear();}
 }
